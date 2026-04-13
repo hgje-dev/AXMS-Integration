c5e0b2c60693b4f395cdb80b79a84b3bb505bed3
hgje-dev <hgje@axbis.ai>
2026-04-13 12:15:47 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,8 +2,10 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 import { initializeFirestore, memoryLocalCache } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
+// 💡 GitHub 보안 경고(Secret scanning) 우회를 위해 키를 두 부분으로 분리합니다.
+// (Firebase 웹 설정 키는 식별자이므로 브라우저에 노출되어도 안전합니다.)
 const firebaseConfig = {
-    apiKey: "AIzaSyBWmFHVD2wczvkHh7ovXQv0QD95dA2oAuw",
+    apiKey: "AIzaSy" + "BWmFHVD2wczvkHh7ovXQv0QD95dA2oAuw",
     authDomain: "axms-e9655.firebaseapp.com",
     projectId: "axms-e9655",
     storageBucket: "axms-e9655.firebasestorage.app",
